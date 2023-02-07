b2f1c5149b32fdc7c03166310fa147a88372d510
InSung <kisn3089@naver.com>
2023-02-07 18:44:32 +0900

    private key

--- src/lib/util/constans.ts
+++ src/lib/util/constans.ts
@@ -1,3 +1,3 @@
 export const BASE_URL = "https://gateway.marvel.com/";
-export const key = "6f42b730d059a41763b9db1783381f38cf1e3cb4";
-// export const key = "0a5bdb1f99257766e70bf2155153f0ff";
+// export const key = "6f42b730d059a41763b9db1783381f38cf1e3cb4";
+export const key = "0a5bdb1f99257766e70bf2155153f0ff";
